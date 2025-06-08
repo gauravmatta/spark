05b862881485e14fe621fb6f51ba3172930cc2e4
Gaurav Matta <gaurav.matta.tech@gmail.com>
2025-06-09 00:16:07 +0530

    Added Logger and RDD

--- first_spark/src/main/java/com/daimplant/first_spark/Main.java
+++ first_spark/src/main/java/com/daimplant/first_spark/Main.java
@@ -3,6 +3,8 @@ package com.daimplant.first_spark;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -17,10 +19,10 @@ public class Main {
 		inputData.add(12.49943);
 		inputData.add(98.32);
 		inputData.add(20.32);
-		
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
 		SparkConf conf = new SparkConf().setAppName("First Spark Application").setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		JavaRDD<Double> myRdd	= sc.parallelize(inputData);
+		JavaRDD<Double> myRdd	= sc.parallelize(inputData);		
 		myRdd.mapToPair(value -> new Tuple2<>(value, value * 2))
 				.foreach(tuple -> System.out.println("Value: " + tuple._1 + ", Double: " + tuple._2));
 		sc.close();
